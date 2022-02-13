ae00714e7502b5f502f757fd1940b1962a24709b
rpatil0722 <90164861+rpatil0722@users.noreply.github.com>
2022-02-13 11:26:35 -0800

    commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const fileServerMiddleware = express.static('public');
+app.use('/', fileServerMiddleware);
+
+app.listen(3000, function () {
+    console.log('Server listening on port 3000');
+});
+        
